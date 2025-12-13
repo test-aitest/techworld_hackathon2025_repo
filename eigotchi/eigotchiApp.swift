332b2fc34de7e02e0c7abe5b1edfb9e8772c766b
koshin <ekoshin2001@icloud.com>
2025-12-13 17:13:57 +0900

    Initial Commit

---
+++ eigotchi/eigotchiApp.swift
@@ -0,0 +1,17 @@
+//
+//  eigotchiApp.swift
+//  eigotchi
+//
+//  Created by 武内公伸 on 2025/12/13.
+//
+
+import SwiftUI
+
+@main
+struct eigotchiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
