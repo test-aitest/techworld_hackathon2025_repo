c4c251899caad5fdf5d56e29574e8803ec0e4956
koshin <ekoshin2001@icloud.com>
2025-12-13 17:13:57 +0900

    Initial Commit

---
+++ eigotchi/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  eigotchi
+//
+//  Created by 武内公伸 on 2025/12/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
